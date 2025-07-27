160d448cd6085b2a7406e67931d20a6a56ca4566
Philip Brocoum <philip.brocoum@gmail.com>
2025-07-26 20:03:19 -0700

    Enhance theme management with localStorage integration
    - Updated `themeAtom` to include custom storage methods for managing theme state in localStorage.
    - Modified `preview-head.html` to save the selected theme in localStorage for persistence across sessions.

--- src/atoms/themeAtom.ts
+++ src/atoms/themeAtom.ts
@@ -1,6 +1,15 @@
 import { atomWithStorage } from "jotai/utils"
 
 // Set the string key and the initial value
-const themeAtom = atomWithStorage("theme", "light")
+const themeAtom = atomWithStorage(
+  "theme",
+  "light",
+  {
+    getItem: (key, initialValue) => localStorage.getItem(key) ?? initialValue,
+    setItem: (key, value) => localStorage.setItem(key, value),
+    removeItem: (key) => localStorage.removeItem(key),
+  },
+  { getOnInit: true },
+)
 
 export { themeAtom }
